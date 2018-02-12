dd481d114e0c939ec5ef5017d93e2b1df03f2321
Johann Behr <johannbehr@exng.de>
2018-02-12 23:48:10 +0100

    Changed about dialog

--- src/common/About.vue
+++ src/common/About.vue
@@ -1,14 +1,76 @@
 <template>
   <div>
-    <!-- TODO: Write about -->
-    <p>Lorem Ipsum</p>
+    <div style="width: 100%; text-align: center;">
+      <div class="btn-group">
+        <button class="btn btn-default"
+                @click="activeTab = 'about'"
+                :class="{ 'active': activeTab === 'about' }"
+        >
+          About
+        </button>
+        <button class="btn btn-default"
+                @click="activeTab = 'dependencies'"
+                :class="{ 'active': activeTab === 'dependencies' }"
+        >
+          Dependencies
+        </button>
+      </div>
+    </div>
+
+    <hr>
+
+    <div style="width: 100%; text-align: center; max-width: 500px;">
+      <div v-show="activeTab === 'about'">
+        <h4>Vuenite <small>(Alpha)</small></h4>
+        <p>Vuenite is a lightweight note app, it aims at simplifying the way of taking notes.</p>
+
+        <br>
+
+        <table>
+          <tr>
+            <td><span class="icon icon-github"></span></td>
+            <td>Source:</td>
+            <td><a>github.com/ExNG/vuenite</a></td>
+          </tr>
+          <tr>
+            <td><span class="icon icon-attention"></span></td>
+            <td>License:</td>
+            <td>MIT</td>
+          </tr>
+          <tr>
+            <td><span class="icon icon-book"></span></td>
+            <td>Author:</td>
+            <td>Johann Behr <a>johannbehr@exng.de</a></td>
+          </tr>
+        </table>
+      </div>
+      <div v-show="activeTab === 'dependencies'">
+        <p>Vuenite only works because of these libraries</p>
+
+        <br>
+
+        <div style="max-height: 50vh; overflow-y: scroll">
+          <table>
+            <tr><td>Quasar Framework</td></tr>
+            <tr><td>VueJS</td></tr>
+            <tr><td>Electron</td></tr>
+            <tr><td>Photonkit</td></tr>
+            <tr><td>MarkdownIt</td></tr>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <hr>
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      activeTab: 'about'
+    }
   }
 }
 </script>
