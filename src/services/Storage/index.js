5d454644c36720fc75da15bb40ac3cba9c8c016f
Johann Behr <johannbehr@exng.de>
2018-02-16 19:36:38 +0100

    Added notice if store would fail

--- src/services/Storage/index.js
+++ src/services/Storage/index.js
@@ -1,10 +1,35 @@
+import { Toast, Dialog } from 'quasar'
+
 export default {
   load (name) {
     return JSON.parse(localStorage.getItem(name))
   },
 
   save (name, value) {
-    localStorage.setItem(name, JSON.stringify(value))
-    return true
+    try {
+      localStorage.setItem(name, JSON.stringify(value))
+      return true
+    }
+    catch (e) {
+      Toast.create.negative({
+        html: 'Could not save your notes!',
+        icon: 'error',
+        timeout: (60 * 1000),
+        button: {
+          label: 'More',
+          handler () {
+            Dialog.create({
+              title: 'Error while saving notes',
+              message: 'The problem is most likely that you ran out of space.',
+              buttons: [
+                'Close'
+              ]
+            })
+          },
+          color: 'white'
+        }
+      })
+      return false
+    }
   }
 }
