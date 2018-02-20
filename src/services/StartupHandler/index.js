c8c317d3b8f367d919ab4d8128c9226a51cf15ab
Johann Behr <johannbehr@exng.de>
2018-02-20 20:30:46 +0100

    Added code documentation to StartupHandler

--- src/services/StartupHandler/index.js
+++ src/services/StartupHandler/index.js
@@ -1,17 +1,30 @@
 import Storage from '../Storage'
 
+
+/**
+ * This handler will run everytime the application starts.
+ * It's there for usefull to make sure all keys are
+ * present before they're called.
+ */
 export default function () {
   let structure = Storage.structure
 
+  // loop over every item
   for (let index in structure) {
     try {
+      // default item value
       let structureData = structure[index]
+
+      // existing userdata, if not set yet it will be by Storage service
       let userData = Storage.load(index)
 
+      // check if it already exists
       if (!userData) {
+        // no, save default value
         Storage.save(index, structureData)
       }
       else {
+        // yes, merge default value with existing data and save it
         let newData = Object.assign(structureData, userData)
         Storage.save(index, newData)
       }
