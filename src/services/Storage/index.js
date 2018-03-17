8466c1839db4dacfb14538e2587624981b17af61
Johann Behr <johannbehr@exng.de>
2018-03-17 11:17:02 +0100

    Removed backup from Storage service

--- src/services/Storage/index.js
+++ src/services/Storage/index.js
@@ -1,8 +1,4 @@
 import Notification from '../Notification'
-import OS from 'os'
-import Path from 'path'
-import FS from 'fs'
-import Moment from 'moment'
 
 export default {
   /**
@@ -213,42 +209,6 @@ export default {
     } catch (e) {
       console.log('Error importing JSON')
       return false
-    }
-  },
-
-  /**
-   * Create file with export JSON.
-   *
-   * @param {String} addittion
-   */
-  backup (addittion = '') {
-    let prefix = process.env.NODE_ENV === 'development' ? 'DEV_' : ''
-
-    // check if vuenite dir exists
-    let vueniteDir = Path.join(OS.homedir(), '.vuenite')
-    if (!FS.existsSync(vueniteDir)) {
-      FS.mkdirSync(vueniteDir)
-    }
-
-    // check if backup dir exists
-    let backupDir = Path.join(vueniteDir, 'backup')
-    if (!FS.existsSync(backupDir)) {
-      FS.mkdirSync(backupDir)
-    }
-
-    if (process.env.NODE_ENV !== 'production') {
-      backupDir = Path.join(backupDir, 'dev')
-      if (!FS.existsSync(backupDir)) {
-        FS.mkdirSync(backupDir)
-      }
-    }
-
-    // check if backup already exists, if not do it (async)
-    let backupFilename = Path.join(backupDir, prefix + Moment().format('YYYY-MM-DD_HH:mm_Z') + addittion + '.json')
-    if (!FS.existsSync(backupFilename)) {
-      FS.writeFile(backupFilename, this.getExportJSON(), (err) => {
-        if (err) throw err
-      })
     }
   }
 }
