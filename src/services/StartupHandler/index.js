6e743fb8d724fd1904ab2095fd873b570eb81e04
Johann Behr <johannbehr@exng.de>
2018-03-17 09:22:35 +0100

    Added backup on every startup

--- src/services/StartupHandler/index.js
+++ src/services/StartupHandler/index.js
@@ -1,4 +1,8 @@
 import Storage from '../Storage'
+import OS from 'os'
+import Path from 'path'
+import FS from 'fs'
+import Moment from 'moment'
 
 /**
  * This handler will run everytime the application starts.
@@ -6,6 +10,28 @@ import Storage from '../Storage'
  * present before they're called.
  */
 export default function () {
+  // check if vuenite dir exists
+  let vueniteDir = Path.join(OS.homedir(), '.vuenite')
+  if (!FS.existsSync(vueniteDir)) {
+    FS.mkdir(vueniteDir)
+  }
+
+  // check if backup dir exists
+  let backupDir = Path.join(vueniteDir, 'backup')
+  if (!FS.existsSync(backupDir)) {
+    FS.mkdir(backupDir)
+  }
+
+  // check if backup already exists, if not do it (async)
+  let backupFilename = Path.join(backupDir, Moment().format('YYYY-MM-DD_HH:MM') + '.json')
+  if (!FS.existsSync(backupFilename)) {
+    FS.writeFile(backupFilename, Storage.getExportJSON(), (err) => {
+      if (err) throw err
+    })
+  }
+
+  console.error('backupFilename', backupFilename)
+
   let structure = Storage.structure
 
   // loop over every item
