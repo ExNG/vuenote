5d6a2973982ddc5a8106ffd9eec51d1e5d0f52ba
Johann Behr <johannbehr@exng.de>
2018-04-02 16:57:34 +0200

    Futureproofed tabs

--- src/services/StartupHandler/index.js
+++ src/services/StartupHandler/index.js
@@ -1,5 +1,6 @@
 import Backup from '../Backup'
 import Storage from '../Storage'
+import Tab from '../Tab'
 
 /**
  * This handler will run everytime the application starts.
@@ -13,22 +14,34 @@ export default function () {
   let structure = Storage.structure
 
   // loop over every item
-  for (let index in structure) {
+  for (let itemName in structure) {
     try {
       // default item value
-      let structureData = structure[index]
+      let structureData = structure[itemName]
 
       // existing userdata, if not set yet it will be by Storage service
-      let userData = Storage.load(index)
+      let userData = Storage.load(itemName)
 
       // check if it already exists
       if (!userData) {
         // no, save default value
-        Storage.save(index, structureData)
+        Storage.save(itemName, structureData)
       } else {
+        // Add missing tab keys
+        if (itemName === 'tabs' && userData.length >= 1) {
+          let newTabs = []
+
+          // Let all tabs go through tab creation
+          for (let tab of userData) {
+            newTabs.push(Tab.add(tab))
+          }
+
+          userData = newTabs
+        }
+
         // yes, merge default value with existing data and save it
         let newData = Object.assign(structureData, userData)
-        Storage.save(index, newData)
+        Storage.save(itemName, newData)
       }
     } catch (e) {
       console.log(e)
