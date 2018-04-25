edb7e6e5e18e9dea7a940dc79052e303ea4c001f
Johann Behr <johannbehr@exng.de>
2018-04-25 15:42:47 +0200

    Created config service

---
+++ src/services/Config/index.js
@@ -0,0 +1,87 @@
+import fs from 'fs'
+import os from 'os'
+import path from 'path'
+
+// import Notification from '../Notification'
+import Essential from '../Essential'
+
+var packageInfo = require('../../../package.json')
+
+var notProduction = process.env.NODE_ENV !== 'production'
+var createDir = (path) => {
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path)
+  }
+}
+
+// Paths
+var dir = path.join(os.homedir(), '.' + packageInfo.name)
+if (notProduction) {
+  dir = path.join(os.homedir(), '.' + packageInfo.name + '_dev')
+}
+createDir(dir)
+
+var backup = path.join(dir, 'backup')
+createDir(backup)
+var data = path.join(dir, 'data')
+createDir(data)
+
+export default {
+  paths: {
+    dir,
+    backup,
+    data
+  },
+
+  /**
+   * Returns all paths.
+   *
+   * @returns {Object}
+   */
+  getPaths () {
+    return Essential.clone(this.paths)
+  },
+
+  /**
+   * Save data to data folder.
+   *
+   * @param {String} name
+   * @param {String} value
+   * @return {Bool}
+   */
+  set (name, value) {
+    let file = path.join(this.paths.data, name)
+
+    if (this._exist(file)) {
+      fs.writeFileSync(file, value)
+      return true
+    } else {
+      return false
+    }
+  },
+
+  /**
+   * Return data saved in data folder.
+   *
+   * @param {String} name
+   * @return {String}||{Bool}
+   */
+  get (name) {
+    let file = path.join(this.paths.data, name)
+
+    if (this._exist(file)) {
+      return fs.readFileSync(file)
+    } else {
+      return false
+    }
+  },
+
+  _exist (path) {
+    if (fs.existsSync(path)) {
+      return true
+    } else {
+      fs.writeFileSync(path, JSON.stringify({}))
+      return true
+    }
+  }
+}
