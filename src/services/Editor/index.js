4b119a6cd36575273fbb4efc97fd9a59e53ccefa
Johann Behr <johannbehr@exng.de>
2018-04-28 18:51:37 +0200

    Moved creation of image preview to own method

--- src/services/Editor/index.js
+++ src/services/Editor/index.js
@@ -31,24 +31,11 @@ export default {
       } else if (/^#### /.test(line)) { // ####
         type = 'h4'
       } else if (/!\[[^\]]+\]\([^\\]+\)/.test(line)) { // image
-        type = 'img'
+        divs.push(this.img(line))
+        continue
       }
 
-      let element = null
-      if (type !== 'img') {
-        element = document.createElement(type)
-      } else {
-        element = document.createElement('div')
-      }
-
-      // Display image if url detected
-      if (type === 'img') {
-        let image = document.createElement('img')
-        image.style.cssText = 'display: block;'
-        let imageSrc = line.match(/http[^)]+/)[0]
-        image.src = imageSrc
-        element.appendChild(image)
-      }
+      let element = document.createElement(type)
 
       // Append text to div
       element.appendChild(document.createTextNode(line))
@@ -56,5 +43,29 @@ export default {
     }
 
     return divs
+  },
+
+  /**
+   * Turn line with image code to image preview and code
+   *
+   * @param {String} line
+   * @return {Element}
+   */
+  img (line) {
+    // Base div containing image and corresponding text
+    let element = document.createElement('div')
+
+    // create image element and set attributes
+    let image = document.createElement('img')
+    image.style.cssText = 'display: block;'
+    image.src = line.match(/http[^)]+/)[0]
+
+    // Append img to base div
+    element.appendChild(image)
+
+    // Append text to base div
+    element.appendChild(document.createTextNode(line))
+
+    return element
   }
 }
