9608b8b6dbc5e19b3a9a405a7afefd93ab154a87
Johann Behr <johannbehr@exng.de>
2018-04-29 18:23:21 +0200

    Created basic gadget component

---
+++ src/components/Gadget.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="gadget">
+    This is a Gadget.
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+#gadget {
+  background-color: rgba(1, 1, 1, 0.02);
+}
+</style>
