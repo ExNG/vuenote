60f81ce6213f5aa32edd3d0dd996a763e64d5f88
Johann Behr <johannbehr@exng.de>
2018-07-27 21:17:10 +0200

    Removed darwin, deb & AppImage

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get install wine zip fuse -y
+sudo apt-get install wine zip -y
 
-# node_modules/quasar-cli/bin/quasar build --mode electron --theme ios --target darwin
 node_modules/quasar-cli/bin/quasar build --mode electron --theme ios --target linux
 
 # NOTE: Saving build files from clean of quasar
@@ -17,14 +16,9 @@ mkdir dist/electron-ios/Vuenote-linux-x64
 cp -r /tmp/Vuenote-linux-x64/* dist/electron-ios/Vuenote-linux-x64
 rm -rf /tmp/Vuenote-linux-x64
 
-# cp src/statics/icon.png dist/electron-ios/Vuenote-darwin-x64/icon.png
 cp src/statics/icon.png dist/electron-ios/Vuenote-linux-x64/icon.png
 cp src/statics/icon.png dist/electron-ios/Vuenote-win32-x64/icon.png
 
-npm run buildDeb
-npm run buildAppImage
-
 cd dist
-# cd electron-ios/Vuenote-darwin-x64/ && zip -r ../../vuenote-darwin-x64.zip ./* -9 && cd ../../
 cd electron-ios/Vuenote-linux-x64/ && zip -r ../../vuenote-linux-x64.zip ./* -9 && cd ../../
 cd electron-ios/Vuenote-win32-x64/ && zip -r ../../vuenote-win32-x64.zip ./* -9 && cd ../../
